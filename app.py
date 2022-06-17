d8c1105a6ef8d2a9bc9965068da904bf794d9db5
David Russell <davidallenr@gmail.com>
2022-06-17 10:36:39 -0400

    Catching up for missed class

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,5 +11,20 @@ def get_index():
 
 
 @app.route("/hello")
-def get_hello():
-    return render_template('hello.html', name="Santa")
+def get_hello(name="Default"):
+    return render_template('hello.html', name=name)
+
+
+@app.route("/login", methods=['GET'])
+def get_login():
+    return render_template('login.html')
+
+
+@app.route("/login", methods=['POST'])
+def post_login():
+    username = request.form.get("username", "<missing name>")
+    password = request.form.get("password", "<missing password>")
+    if password == "password1":
+        return redirect(url_for('get_hello', name=username))
+    else:
+        return redirect(url_for('get_login'))
